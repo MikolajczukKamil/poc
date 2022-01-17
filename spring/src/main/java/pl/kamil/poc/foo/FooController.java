76c8d45e0aba141c878b55fdbebd25eae6809a92
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2022-01-17 21:33:36 +0100

    WTF

--- spring/src/main/java/pl/kamil/poc/foo/FooController.java
+++ spring/src/main/java/pl/kamil/poc/foo/FooController.java
@@ -5,10 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
-
 @RestController
 @RequestMapping(value = "/spring")
 @RequiredArgsConstructor
@@ -18,19 +14,7 @@ public class FooController {
     @GetMapping("/test")
     public FooEntity test() {
         try {
-            var entity = fooRepository.getById(1);
-
-            TimeUnit.SECONDS.sleep(2);
-
-            if (entity.getList() == null) {
-                 entity.setList(new ArrayList<>());
-            }
-
-             entity.getList().add(new FooDataEntity(null, LocalDateTime.now().toString(), entity));
-
-             fooRepository.save(entity);
-
-            return entity;
+            return fooRepository.getById(1);
         }
         catch (Exception e) {
             return null;
